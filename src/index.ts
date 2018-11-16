c2020dfb35bb8d7c15c169403c1953cddedb670d
Dariusz <dariusz@grabka.org>
2018-11-15 23:01:50 -0500

    updated dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import { Searcher } from "./searcher"
 import { SearchResults } from "./searchresults";
-import { Rankings } from "./rankings";
 
 const s = new Searcher()
 const sr = new SearchResults()
 
-s.search('nba power rankings', 50, 'm').then(
-    (results) => {
-        sr.loadRankings(results)
-    },
-    (err) => console.log("Error searching: " + err)
-)
+s.search('nba power rankings', 50, 'm')
+    .then(
+        (results) => {
+            sr.loadRankings(results)
+        },
+        (err) => {
+            console.log("Error searching: " + err)
+        }
+    )
