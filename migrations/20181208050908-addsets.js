4d6cdb456df29a9d60332fb548b395b8b1b562ea
Dariusz <dariusz@grabka.org>
2018-12-08 00:22:20 -0500

    first step to add db migs

---
+++ migrations/20181208050908-addsets.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.createTable('sets', {
+    columns: {
+      id: { type: 'int', primaryKey: true, autoIncrement: true },
+      url: 'string',
+      parser: 'string',
+      title: 'string',
+      description: 'string',
+      date_published: 'datetime',
+      date_created: { type: 'datetime', defaultValue: new Date().toISOString() }
+    },
+    ifNotExists: true
+  });
+};
+
+exports.down = function(db) {
+  return db.dropTable('sets', {
+    ifExists: true
+  })
+};
+
+exports._meta = {
+  "version": 1
+};
